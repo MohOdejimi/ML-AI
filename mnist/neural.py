dbdaf9012180057c90ec2d25aa8e366a4038c929
mohodejimi <mohammedodejimi@gmail.com>
2026-08-26 12:35:47 +0100

    Used 10 epochs to train the 60000 samples from the MNIST dataset and visualized the loss figures from each trainig test with the matplotlib library

--- mnist/neural.py
+++ mnist/neural.py
@@ -1,5 +1,6 @@
 import torch 
-import numpy 
+import numpy as np
+import matplotlib.pyplot as plt 
 import torchvision.datasets as datasets
 from torchvision import transforms
 from torch.utils.data import DataLoader
@@ -8,19 +9,16 @@ from torch import optim
 
 
 # CREATE TRAIN AND TEST SAMPLES 
-
 mnist_train = datasets.MNIST(root='./data', train = True, download = True, transform = transforms.ToTensor())
 mnist_test = datasets.MNIST(root = './data', train = False, download = True, transform = transforms.ToTensor())
 
 # LOAD TRAIN AND TEST SAMPLES WITH DATALOADER
-
 batch_size = 128 
 
 mnist_traindataloader = DataLoader(dataset = mnist_train, batch_size = batch_size, shuffle = True)
 mnist_testdataloader = DataLoader(dataset = mnist_test, batch_size = batch_size, shuffle = False)
 
 # CREATE NEURAL NETWORK
-
 in_features = 784 
 hidden_states = 128
 output = 10 
@@ -43,9 +41,43 @@ class NeuralNetwork(nn.Module):
 
 model = NeuralNetwork(in_features, hidden_states, output)
 
+# TRAINING MODEL 
 learning_rate = 0.01
 loss_fn = nn.CrossEntropyLoss()
 optimizer = optim.SGD(model.parameters(), learning_rate)
+
+num_epochs = 10
+loss_values = []
+
+for epoch in range(num_epochs):
+    for tensor, label in mnist_traindataloader:
+        optimizer.zero_grad()
+
+        predicted = model(tensor)
+        loss = loss_fn(predicted, label)
+        loss_values.append(loss.item())
+        loss.backward()
+        optimizer.step()
+
+
+step = range(len(loss_values))
+fig, ax = plt.subplots(figsize=(8,5))
+plt.plot(step, np.array(loss_values))
+plt.title("Step-wise Loss")
+plt.xlabel("Training Steps")
+plt.ylabel("Loss")
+
+plt.savefig("loss.png")
+ 
+
+test_instances = len(mnist_test)
+
+# TESTING MODEL 
+
+num_pred = []
+num_target = []
+correct = 0 
+total = 0 
 
 
 
